53af3be5650f79e7495fd40b0ed6124e1ab787c6
tearatio <tearatio@gmail.com>
2021-03-01 18:42:50 +0200

    Svelte_testing

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	let password = {pass: '', id: ''}
+	let array = []
+	
+	function passHandler() {
+		
+		if (password.pass.length >= 5 && password.pass.length <=10) {array = [...array, 
+			{pass: this.password, id: Math.random()}
+		]
+		}
+		
+		password.pass = ''
+	}
+</script>
+
+<main>
+	<div>
+		<input type="password" bind:value="{password.pass}">
+		{#if password.pass.length < 5}
+			<p>Too short</p>
+			{:else if password.pass.length > 10}
+			<p>Too long</p> 
+	{:else} <p>{password.pass}</p>
+		{/if}
+	</div>
+	<button on:click="{passHandler}">Save password to the array</button>
+	<hr>
+	<div>
+		The enetered passwords are the following: {#each array as item}
+			<ul>{item.pass}</ul>
+			{console.log(item.pass)}
+		{/each}
+	</div>
+</main>
+
